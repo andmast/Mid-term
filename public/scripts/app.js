e03b4f5d0f01aec0ebf58b3f20cb64a377682312
Andrea <anmastrantoni@gmail.com>
2019-04-26 14:39:41 +0000

    delete added to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -111,6 +111,20 @@ const loadItems = function() {
 $(document).ready(function() {
 
 
+$("#delete").on("click", function( event ) {
+  event.preventDefault();
+  $.ajax({
+     url: '/api/users',
+     type: 'DELETE',
+     success: function(response) {
+      alert("success");
+     }
+    })
+  });
+
+
+
+
 
   // createItem(data);
   loadItems();
